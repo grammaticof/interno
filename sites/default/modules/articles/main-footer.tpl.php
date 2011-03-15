b1be21db733cd7aeeaf55f7d6f495a2350d2f1a0
Francesco Grammatico <grammatico.francesco@gmail.com>
2011-03-15 17:51:52 +0000

    latest changes

---
+++ sites/default/modules/articles/main-footer.tpl.php
@@ -0,0 +1,11 @@
+<div class="info">
+	Associazione ‘interno18’ - Caserta CF 93065040615 tel. 3297943224/3282656347 fax 0823847165	
+</div>
+
+<div class="reg">
+	Testata giornalistica registrata presso il Tribunale di Santa Maria Capua Vetere (Caserta) con num. 762 del 07/05/2010
+</div>
+
+<div class="cc">
+	<p>L'utilizzo del contenuto di questo articolo è soggetto alle condizioni della Licenza Creative Commons. Sono consentite la distribuzione, la riproduzione e la realizzazione di opere derivate per fini non commerciali, purchè venga citata la fonte.</p>
+</div>
