ba39955c5831102952f1f8628e2f3200b24922fc
Francesco Grammatico <grammatico.francesco@gmail.com>
2011-04-07 15:57:00 +0100

    other mods

--- sites/default/modules/firstpage/block--firstpage.tpl.php
+++ sites/default/modules/firstpage/block--firstpage.tpl.php
@@ -42,14 +42,14 @@
  * @see template_process()
  */
 ?>
+
+test
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-
   <?php print render($title_prefix); ?>
-<?php if ($block->subject): ?>
-  <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
-<?php endif;?>
+    <?php if ($block->subject): ?>
+      <h2<?php print $title_attributes; ?>><a url="<?php print $block->url ?>"><?php print $block->subject ?></a></h2>
+    <?php endif;?>
   <?php print render($title_suffix); ?>
-
   <div class="content"<?php print $content_attributes; ?>>
     <?php print $content ?>
   </div>
