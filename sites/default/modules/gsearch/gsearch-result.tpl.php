d9a13d2b83a949ed8edf2f0d59b9cf75c3f9025c
Francesco Grammatico <grammatico.francesco@gmail.com>
2011-04-11 00:58:48 +0100

    some js js

--- sites/default/modules/gsearch/gsearch-result.tpl.php
+++ sites/default/modules/gsearch/gsearch-result.tpl.php
@@ -1,5 +1,6 @@
 <div id="cse-search-results"></div>
 <script type="text/javascript">
+<!--//--><![CDATA[//><!--
 	var googleSearchIframeName = "cse-search-results";
 	var googleSearchFormName = "cse-search-box";
 	var googleSearchResizeIframe = false;
@@ -7,4 +8,5 @@
 	var googleSearchDomain = "www.google.com";
 	var googleSearchPath = "/cse";
 </script>
+//--><!]]>
 <script type="text/javascript" src="http://www.google.com/afsonline/show_afs_search.js"></script>
