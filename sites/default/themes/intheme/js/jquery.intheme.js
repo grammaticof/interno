526d7796e6e6f22858de680ecc16d3e34fdbd123
Francesco Grammatico <grammatico.francesco@gmail.com>
2011-04-20 17:16:46 +0100

    news

--- sites/default/themes/intheme/js/jquery.intheme.js
+++ sites/default/themes/intheme/js/jquery.intheme.js
@@ -16,5 +16,46 @@
       }).find('ul').bgIframe({opacity:false});
     }
   };
+  
+  Drupal.behaviors.submenu = {
+    attach: function(context) {
+      var timeInterval = 10000;
+      var $active;
+      var $next;
+      
+      function swapInfo() {
+        $active = $('#block-user-toolbar-sub-menu ul.current');
+        $next = ($('#block-user-toolbar-sub-menu ul.current').next().length > 0) ? $('#block-user-toolbar-sub-menu ul.current').next() : $('#block-user-toolbar-sub-menu ul:first');
+        
+        $next.fadeIn(function() {
+          $active.fadeOut().removeClass('current');
+          $next.addClass('current');
+        });
+      }
+      // autoplay
+      var interval = setInterval(swapInfo, timeInterval);
+      
+      // pause/play
+      $('#block-user-toolbar-sub-menu').hover(function () {
+        clearInterval(interval);
+      }, function () {
+        interval = setInterval(swapInfo, timeInterval);
+      });
+      
+      /*
+      $('ul.main-menu').hover(function () {
+        clearInterval(interval);
+        $('#block-user-toolbar-sub-menu ul:first').fadeIn( function() {
+          $('#block-user-toolbar-sub-menu ul.current').fadeOut().removeClass('current');
+          $('#block-user-toolbar-sub-menu ul:first').addClass('current');
+        });
+
+      }, function() {
+        interval = setInterval(swapInfo, timeInterval);
+      });
+      */
+    }
+  };
+  
 
 })(jQuery);
--- sites/default/themes/intheme/template.php
+++ sites/default/themes/intheme/template.php
@@ -17,9 +17,86 @@
  * Implements hook_theme().
  */
 function intheme_theme(&$existing, $type, $theme, $path) {
-  $hooks = array();
+  return array(
+		'item_list_no_div' => array(
+			'variables' => array(
+				'items' => NULL,
+				'title' => NULL,
+				'type' => 'ul',
+			),
+		),
+	);
+}
+
+/**
+ * Returns HTML for a list or nested list of items.
+ *
+ * @param $variables
+ *   An associative array containing:
+ *   - items: An array of items to be displayed in the list. If an item is a
+ *     string, then it is used as is. If an item is an array, then the "data"
+ *     element of the array is used as the contents of the list item. If an item
+ *     is an array with a "children" element, those children are displayed in a
+ *     nested list. All other elements are treated as attributes of the list
+ *     item element.
+ *   - title: The title of the list.
+ *   - type: The type of list to return (e.g. "ul", "ol").
+ *   - attributes: The attributes applied to the list element.
+ */
+function intheme_item_list_no_div($variables) {
+  $items = $variables['items'];
+  $title = $variables['title'];
+  $type = $variables['type'];
+
+  $attributes = $variables['attributes'];
   
-  return $hooks;
+  $output = '';
+  
+  
+  if (isset($title)) {
+    $output .= '<h3>' . $title . '</h3>';
+  }
+
+  if (!empty($items)) {
+    $output .= "<$type" . drupal_attributes($attributes) . '>';
+    $num_items = count($items);
+    foreach ($items as $i => $item) {
+      $attributes = array();
+      $children = array();
+      if (is_array($item)) {
+        foreach ($item as $key => $value) {
+          if ($key == 'data') {
+            $data = $value;
+          }
+          elseif ($key == 'children') {
+            $children = $value;
+          }
+          else {
+            $attributes[$key] = $value;
+          }
+        }
+      }
+      else {
+        $data = $item;
+      }
+      if (count($children) > 0) {
+        // Render nested list.
+        $data .= theme_item_list(array('items' => $children, 'title' => NULL, 'type' => $type, 'attributes' => $attributes));
+      }
+      if ($i == 0) {
+        $attributes['class'][] = 'first';
+      }
+      if ($i == $num_items - 1) {
+        $attributes['class'][] = 'last';
+      }
+      $output .= '<li' . drupal_attributes($attributes) . '>' . $data . "</li>\n";
+    }
+    $output .= "</$type>";
+  }
+  
+  $output .= '';
+
+  return $output;
 }
 
 
