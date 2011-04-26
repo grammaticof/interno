86b49b1eb4d149d55142b6b73954dbac35fbe285
Francesco Grammatico <grammatico.francesco@gmail.com>
2011-04-26 16:42:04 +0200

    a lot of adsense

---
+++ sites/default/modules/facebook/adsense-box.tpl.php
@@ -0,0 +1,15 @@
+<div class="adsense">
+	<center>
+		<script type="text/javascript"><!--
+		google_ad_client = "ca-pub-6602237752552603";
+		/* interno18v2 - 468x60 - art */
+		google_ad_slot = "0509506240";
+		google_ad_width = 468;
+		google_ad_height = 60;
+		//-->
+		</script>
+		<script type="text/javascript"
+		src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+		</script>
+	</center>
+</div>
